cf47d043103963c56c8b99a5f938b9c622f3d6da
Erkan Öz <zo.nakre@gmail.com>
2024-10-09 12:21:40 +0300

    Deployment config for railway.app

--- medusa-config.js
+++ medusa-config.js
@@ -11,6 +11,48 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE,
+    redistUrl: process.env.REDIS_URL
+ },
+ admin: {
+  path: process.env.ADMIN_PATH || '/app',
+  disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+  backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000"
+ },
+ modules: {
+  [Modules.FILE]: {
+    resolve: '@medusajs/file',
+    options: {
+      providers: [
+        {
+          resolve: '@medusajs/file-local-next',
+          id: 'local',
+          options: {
+            backend_url: `${process.env.MEDUSA_BACKEND_URL}/static`
+          }
+        }
+      ]
+    }
+  },
+  [Modules.CACHE]: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: process.env.REDIS_URL
+    }
+  },
+  [Modules.EVENT_BUS]: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: process.env.REDIS_URL
+    }
+  },
+  [Modules.WORKFLOW_ENGINE]: {
+    resolve: "@medusajs/workflow-engine-redis",
+    options: {
+      url: process.env.REDIS_URL
     }
   }
+ }
 })
+
